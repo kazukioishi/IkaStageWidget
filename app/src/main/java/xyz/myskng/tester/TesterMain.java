944acce683131d427966a21c522fdd0231fb55fd
Kazuki Oishi <oishikazuki@gmail.com>
2016-05-26 17:50:47 +0900

    次のステージの取得機能実装

--- app/src/main/java/xyz/myskng/tester/TesterMain.java
+++ app/src/main/java/xyz/myskng/tester/TesterMain.java
@@ -1,7 +1,10 @@
 package xyz.myskng.tester;
 
 import xyz.myskng.ikastagewidget.api.StageInfo;
+import xyz.myskng.ikastagewidget.model.GachiStage;
 import xyz.myskng.ikastagewidget.model.Stage;
+
+import java.util.ArrayList;
 
 public class TesterMain {
     public static void main(String args[]){
@@ -13,5 +16,8 @@ public class TesterMain {
         Stage stg2 = StageInfo.GetGachiStageList();
         System.out.println(stg2.getMaps()[0]);
         System.out.println(stg2.getMaps()[1]);
+        ArrayList<Stage> ar = StageInfo.GetNextRegularStageList();
+        ArrayList<GachiStage> ar2 = StageInfo.GetNextGachiStageList();
+        System.out.println("complete.");
     }
 }
--- app/src/main/kotlin/xyz/myskng/ikastagewidget/api/StageInfo.kt
+++ app/src/main/kotlin/xyz/myskng/ikastagewidget/api/StageInfo.kt
@@ -7,11 +7,14 @@ import okhttp3.Request
 import xyz.myskng.ikastagewidget.model.GachiStage
 import xyz.myskng.ikastagewidget.model.Stage
 import java.text.SimpleDateFormat
+import java.util.*
 
 class StageInfo {
     companion object{
         val GACHI_STAGE_INFO_API_ENDPOINT : String = "http://splapi.retrorocket.biz/gachi/now"
+        val GACHI_STAGE_INFO_API_NEXT_ENDPOINT : String = "http://splapi.retrorocket.biz/gachi/next_all"
         val REGULAR_STAGE_INFO_API_ENDPOINT : String = "http://splapi.retrorocket.biz/regular/now"
+        val REGULAR_STAGE_INFO_API_NEXT_ENDPOINT : String = "http://splapi.retrorocket.biz/regular/next_all"
         val API_TIMEFORMAT : SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'kk:mm:ss")
 
         @JvmStatic fun GetGachiStageList() : GachiStage {
@@ -39,6 +42,41 @@ class StageInfo {
             stg.startTime = API_TIMEFORMAT.parse(node.at("/result")[0].at("/start").asText())
             stg.endTime = API_TIMEFORMAT.parse(node.at("/result")[0].at("/end").asText())
             return stg
+        }
+
+        @JvmStatic fun GetNextGachiStageList() : ArrayList<GachiStage>{
+            var client : OkHttpClient = OkHttpClient()
+            var req : Request.Builder = Request.Builder()
+            req.url(GACHI_STAGE_INFO_API_NEXT_ENDPOINT)
+            var json : String =  client.newCall(req.build()).execute().body().string()
+            var node : JsonNode = ObjectMapper().readTree(json)
+            var stagelist : ArrayList<GachiStage> = ArrayList()
+            node.at("/result").forEach {
+                var stg : GachiStage = GachiStage()
+                stg.maps = arrayOf(it.at("/maps")[0].asText(),it.at("/maps")[1].asText())
+                stg.startTime = API_TIMEFORMAT.parse(it.at("/start").asText())
+                stg.endTime = API_TIMEFORMAT.parse(it.at("/end").asText())
+                stg.rule = it.at("/rule").asText()
+                stagelist.add(stg)
+            }
+            return stagelist
+        }
+
+        @JvmStatic fun GetNextRegularStageList() : ArrayList<Stage>{
+            var client : OkHttpClient = OkHttpClient()
+            var req : Request.Builder = Request.Builder()
+            req.url(REGULAR_STAGE_INFO_API_NEXT_ENDPOINT)
+            var json : String =  client.newCall(req.build()).execute().body().string()
+            var node : JsonNode = ObjectMapper().readTree(json)
+            var stagelist : ArrayList<Stage> = ArrayList()
+            node.at("/result").forEach {
+                var stg : Stage = Stage()
+                stg.maps = arrayOf(it.at("/maps")[0].asText(),it.at("/maps")[1].asText())
+                stg.startTime = API_TIMEFORMAT.parse(it.at("/start").asText())
+                stg.endTime = API_TIMEFORMAT.parse(it.at("/end").asText())
+                stagelist.add(stg)
+            }
+            return stagelist
         }
     }
 }
