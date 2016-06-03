d202a0a0daa330beb7debe3f655f2c3f8813ce72
Kazuki Oishi <oishikazuki@gmail.com>
2016-06-03 10:38:30 +0900

    ステージ名とリソースを対応した

---
+++ app/src/kotlintester/xyz/myskng/ikastagewidget/StageImageTester.kt
@@ -0,0 +1,12 @@
+package xyz.myskng.ikastagewidget
+
+import xyz.myskng.ikastagewidget.ui.StageImage
+
+class StageImageTester {
+    companion object{
+        @JvmStatic fun main(args : Array<String>){
+            println(StageImage.stageHashMap["デカライン高架下"])
+            println(StageImage.stageHashMap["デカライン高架"])
+        }
+    }
+}
